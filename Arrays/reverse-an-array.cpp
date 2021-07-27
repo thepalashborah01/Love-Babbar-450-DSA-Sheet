1aad1c2049e47cd8e24403e9001538e89f5190b1
Palash Protim Borah <palashborah04@gmail.com>
2021-07-27 23:13:54 +0530

    last edit : 27.07.21

---
+++ Arrays/reverse-an-array.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+ {
+	int test; cin>>test;
+	while(test--)
+	{
+	    int n; cin>>n;
+	
+	    int arr[n];
+    	for(int i=0; i<n; i++)
+	        cin>>arr[i];
+	
+	    for(int i=n-1; i>=0; i--)
+	        cout<<arr[i]<<" ";
+	
+	    cout<<"\n";
+	}
+	return 0;
+}
