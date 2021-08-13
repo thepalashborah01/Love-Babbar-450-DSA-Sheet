cd4ae1e349917a285c2dedb998256500546cc79a
Palash Protim Borah <palashborah04@gmail.com>
2021-08-13 21:11:41 +0530

    last edit : 13.08.21

---
+++ Arrays/merge-interval.cpp
@@ -0,0 +1,9 @@
+//brute force: sort, linearly itterate and see, which merges, and take another data structure and store 1st and 2nd of each element
+// O(nlogn)+O(n^2)
+
+//sort it, and store the first element in a pair, and check merging and add them in the data structure while chanign the pair value
+//O(nlogn)+O(n)
+
+//https://leetcode.com/problems/merge-intervals/submissions/
+
+//Link to solution
